c698f19b23f1208d04359a0747de7a1846516d3c
Asyurazhr <Asyuradjola2004@gmail.com>
2026-07-15 16:02:12 +0700

    add Profil

---
+++ src/main/java/com/deploy/pertemuan11/model/Profile.java
@@ -0,0 +1,21 @@
+package com.deploy.pertemuan11.model;
+
+
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@Table(name = "profile")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Profile {
+
+}
+
